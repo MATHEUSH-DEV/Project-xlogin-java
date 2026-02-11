40c4c1152b3c462534bbb043d2d71b6248a36b74
Matheus H. <mattuza@yahoo.com>
2026-02-11 14:44:05 -0300

    Update Xregister.java

--- Xregister.java
+++ Xregister.java
@@ -4,14 +4,14 @@ import javax.swing.JLabel;
 public class Xregister {
     
     public void abrirTela() {
-        JFrame frame = new JFrame("X Register");
+        JFrame frame = new JFrame("Kronus Register v1.0");
         frame.setSize(550, 300);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // DISPOSE fecha apenas esta janela
         frame.setLayout(null);
         frame.setResizable(false);
 
         // elementos
-        JLabel label1 = new JLabel("REGISTRO v1.0");
+        JLabel label1 = new JLabel("Kronus REGISTRO v1.0");
         
 
         // Adicione seus componentes do registro aqui...
@@ -20,10 +20,10 @@ public class Xregister {
         frame.add(label1);
 
         // tamanho JLabel
-        label1.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 25));
+        label1.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 27));
 
         // tamanho
-        label1.setBounds(80, 20, 250, 40);
+        label1.setBounds(100, 20, 350, 40);
 
         frame.setLocationRelativeTo(null); 
         frame.setVisible(true);
