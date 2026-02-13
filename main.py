ccf23c167e26c8048f9e7dc7679cdef59e67a7eb
MATHEUSH-DEV <mattuza@yahoo.com>
2026-02-12 21:41:02 -0300

    update alpha

---
+++ main.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+def iniciar_banco():
+    # Conecta ao arquivo (se não existir, ele cria na hora)
+    conn = sqlite3.connect('kronus_local.db')
+    cursor = conn.cursor()
+
+    # Criando a tabela de usuários
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            status TEXT DEFAULT 'OFFLINE'
+        )
+    ''')
+    conn.commit()
+    return conn
+
+def registrar_usuario(username, password):
+    conn = iniciar_banco()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO usuarios (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        print(f"[SUCCESS] Usuário {username} registrado localmente!")
+    except Exception as e:
+        print(f"[ERROR] Falha ao registrar: {e}")
+    finally:
+        conn.close()
+
+# Executando
+user = input("Novo username: ")
+pwd = input("Nova senha: ")
+registrar_usuario(user, pwd)
